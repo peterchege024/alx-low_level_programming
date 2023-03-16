860fc126ed9600bfa097f3dfec1af14b42eab688
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-17 02:27:45 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-timo
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
