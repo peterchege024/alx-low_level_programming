9c87000312c7719c4d12cde1e2a774e7da3046d3
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-18 06:01:43 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,23 @@
-9-print_comb.c
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int i = '0';
+while (i <= '9')
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+++i;
+}
+putchar('\n');
+return (0);
+}
