39a5ccb7237b00c4408605370c73f43afb24c7a0
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-18 04:27:31 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,28 @@
-1-last_digit.c
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Determine whether random number is negatif or positive
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
+int lastnum;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastnum = n % 10;
+if (lastnum < 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastnum);
+}
+else if (lastnum == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastnum);
+}
+else if (lastnum < 6 && lastnum != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and mot 0\n", n, lastnum);
+}
+return (0);
+}
