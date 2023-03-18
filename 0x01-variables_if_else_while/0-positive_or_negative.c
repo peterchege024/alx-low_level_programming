d018da33557850d2408de4b41de695ca6b170a22
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-18 03:45:01 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,29 @@
-0-positive_or_negative.c
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Determine whether random number is negatif or positive
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n<0)
+  {
+    printf("%d is %\n", n, "negative");
+    
+  }
+  els if (n>0)
+  {
+    printf("%d is %\n", n, "positive");
+    
+  }
+  else
+  {
+    printf("%d is %\n", n, "zero");
+    
+  }
+return (0);
+}
