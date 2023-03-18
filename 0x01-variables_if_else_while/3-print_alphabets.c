faebc0b0a75e42c3587915a541dc1c60b4e58cf2
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-18 05:09:45 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,17 @@
-3-print_alphabets.c
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int lowerCase = 'a';
+int upperCase = 'A';
+while (lowerCase <= 'z')
+{
+putchar(upperCase);
+upperCase += 1;
+}
+putchar('\n');
+return (0);
+}
