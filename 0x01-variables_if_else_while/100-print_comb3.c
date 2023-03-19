035c35c8b14ae0b6ac47a66edf7d099c42e3e112
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-19 09:47:06 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,36 @@
-100-print_comb3.c
+#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: 0 (Success)
+*/
+int main(void)
+{
+int n = '0';
+int m = '0';
+while (n <= '9')
+{
+while (m <= '9')
+{
+if (!(n > m) || n==m)
+{
+putchar(n);
+putchar(m);
+if (n == '8' && m == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+m++;
+}
+m = '0';
+n++;
+}
+}
+return (0);
+}
