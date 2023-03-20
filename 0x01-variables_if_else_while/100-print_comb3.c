734de7159a7dc981d917060460ab7ef0d2c322ed
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-20 12:13:01 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*Return: 0 (Success)
-*/
+ * main - print numbers from 00 to 99.
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-int n = '0';
-int m = '0';
-while (n <= '9')
-{
-while (m <= '9')
-{
-if (!(n > m) || n == m)
-{
-putchar(n);
-putchar(m);
-if (n == '8' && m == '9')
-{
-putchar('\n');
-}
-else
-{
-putchar(',');
-putchar(' ');
-}
-}
-m++;
-}
-m = '0';
-n++;
-}
-return (0);
+	int n = '0';
+	int m = '0';
+
+	while (n <= '9')
+	{
+		while (m <= '9')
+		{
+			if (!(n > m || n == m))
+			{
+				putchar(n);
+				putchar(m);
+				if (n == '8' && m == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			m++;
+		}
+		m = '0';
+		n++;
+	}
+	return (0);
 }
