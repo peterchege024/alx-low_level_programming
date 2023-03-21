823351ec66e7dfbf674f395537a76311c33e873c
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-21 23:28:57 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - nestet loops.
+ * main - entry point.
  *
  * Return: 0 on success
  */
-int _putchar(char c)
+int main(void)
 {
-	
-	return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
