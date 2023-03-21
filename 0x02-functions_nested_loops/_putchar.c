f0acbe0492dbce47e6ffc066150b6aa4ff93af02
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-21 23:39:37 +0300

    Update _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - nestet loops.
- *
- * Return: 0 on success
+ * _putchar - writes the character c to stdout.
+ * @c: The character to print.
+ * Return: on success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
