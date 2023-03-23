88979ce86389c829a9759409147d91663d084f19
peterchege024 <peterchege024@gmail.com>
2023-03-23 15:31:04 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - make the alpha bet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
