d985892c2411d6c80d40c5e4f9ba859d245c107a
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-27 18:53:57 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
-* print_sign - prints the sign of a number n.
+* print_sign - prints the sign of a number.
+* 
 * @c: The number to be checked.
 * Return: 1 for positive num, -1 for negative num or zero for anything else.
 */
-int print_sign(int n)
+int print_sign(int c)
 {
-if (n > 0)
+if (c > 0)
 {
 _putchar(43);
 return (1);
 }
-else if (n < 0)
+else if (c < 0)
 {
 _putchar(45);
 return (-1);
