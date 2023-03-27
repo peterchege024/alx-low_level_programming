4345a0782f9a4b4a4bcde0dcc563fb5ac4677595
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-27 18:16:42 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
 *@c: The character to check.
 *Return: 1 for lowercase character or 0 for anything else
 */
-int _isalphs(int c)
+int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 {
