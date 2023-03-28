5a0d960868b54b3d212b3d6fbfed414ac9d02ac3
peterchege024 <peterchege024@gmail.com>
2023-03-27 19:35:49 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* reset_to_98 - takes piter to an int and updates the values it points to 98.
+*@n: An integer input pointer.
+*Return: nothing.
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
