c3f31bdf384c572a6efc97ee0a941b937b9c2876
peterchege024 <peterchege024@gmail.com>
2023-03-27 21:40:54 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* _strlen - returns length of a string
+*@str: String to be checked.
+*Return: Length of str.
+*/
+size_t _strlen(const char *str)
+{
+size_t length = 0;
+while (@str++)
+length++;
+return (length);
+}
