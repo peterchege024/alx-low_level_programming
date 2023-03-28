82295087e15ae0858d75ba6a8b6a24d7871f2544
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-03-28 12:43:03 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* add - add two values, k, l.
-* @nk: the value n1 to be computed
-* @nl: the value n2 to be computed
+* add - add two values, k and l.
+* @k: the value k to be computed
+* @l: the value l to be computed
 * Return: integer.
 */
 int add(int k, int l)
