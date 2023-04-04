6bde9f83be4ef10962369c5e37fccb1c31573f78
peterchege024 <peterchege024@gmail.com>
2023-04-04 13:31:14 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*more_numbers - print numbers..
+*Return: print 10 times a num..
+*/
+void more_numbers(void)
+{
+int x;
+int y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_line - draw straight line.
+*@n: nmber of lines
+*Return: empty.
+*/
+void print_line(int n)
+{
+int k;
+if (k <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (k = 0; k < n; k++ )
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
