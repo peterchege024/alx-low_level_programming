44288c79498c6ebd985ee152d5c336e58f1c95b6
peterchege024 <peterchege024@gmail.com>
2023-04-04 11:38:12 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isupper - check if a letter is super.
+*@c: the num ber to be checked.
+*Return: 1 for upper letter and 0 for otherwise.
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
