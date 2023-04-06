208efc13eebbcb9f86916ca6d1e7c9884460e51e
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-06 15:44:03 +0300

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_sqrt_recursion - returns square root of a number.
+*@n: the number.
+*@k: the iteration number
+*Return: squreroot.
+*/
+int _sqrt_recursion(int n)
+{
+int sqrt = k * k;
+if (sqrt > n)
+return (-1);
+if (sqrt == n)
+return (k);
+return (_sqrt(n, k + 1));
+}
