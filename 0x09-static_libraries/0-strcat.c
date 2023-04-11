9b2be91b04b9e1b72489fc64605adf108bc8c302
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 12:13:44 +0300

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_strcat - concatenate two strings.
+*@dest: destination string pointer.
+*@src: source string pointer
+* Return: pointer to destination string.
+*/
 char *_strcat(char *dest, char *src)
 {
 return strcat(dest, src);
