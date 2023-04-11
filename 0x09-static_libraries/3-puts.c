a790c42cc9b3d42ce38fc17cf32a75014849fb53
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 13:16:05 +0300

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+* _puts - prints a string
+*@str: the string to be printed.
+* Return: void.
+*/
 void _puts(char *s)
 {
 puts(s);
