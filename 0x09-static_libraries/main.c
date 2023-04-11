7b17255954200e8029a4896f2f757763c06ce9b9
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 14:50:52 +0300

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-_puts("At the end of the day, my goal was to be the best hacker\nKevin Mitnick");
+_puts("At the end of the day,my goal was to be the besthacker\nKevin Mitnick");
 return (0);
 }
