2bbdb183db89bcb07320314beed96b6a78c4729e
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 12:08:40 +0300

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_memset - Entry point.
+*@s: pointer directions.
+*@b: constant byte.
+*@n: byte.
+*Return: 0.
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
 return memset(s, b, n);
