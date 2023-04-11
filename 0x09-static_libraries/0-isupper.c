dca3cd6b07ba42af36a26217fd0c2c7d286ad1f5
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 14:10:10 +0300

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_isupper - check if a letter is super.
+*@c: the num ber to be checked.
+*Return: 1 for upper letter and 0 for otherwise.
+*/
 int _isupper(int c)
 {
 return (c >= 'A' && c <= 'Z');
