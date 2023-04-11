f25e4c1e8467f62c6e608fcee18d95d39d1b63a8
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 13:26:34 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_strspn - Entry point.
+*@s: input.
+*@accept: input
+*Return: 0.
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-return strspn(s, accept);
+return (strspn(s, accept));
 }
