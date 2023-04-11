305af0e6097373217e6fa870296468bc6707842a
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 12:49:18 +0300

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_strncat - concatenate two strings.
+*@dest: destination string pointer.
+*@src: source string pointer.
+*@n: number of bytes concatenated.
+* Return: pointer to destination string.
+*/
 char *_strncat(char *dest, char *src, int n)
 {
 return strncat(dest, src, n);
