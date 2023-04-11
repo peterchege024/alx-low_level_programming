4f5630b28463ed673312b0520127465fba74f1bc
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 13:03:34 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_strncpy - counting bytes.
+*@dest: destination string pointer.
+*@src: source string pointer.
+*@n: number of bytes counted.
+* Return: number of byte count.
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-return strncpy(dest, src, n);
+return (strncpy(dest, src, n));
 }
