157a687ee0768b59a54dfeebc13be1cb4e3f0fc9
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 12:09:51 +0300

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_memcpy - Entry point.
+*@dest: input.
+*@src: input.
+*@n: input.
+*Return: 0.
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 return memcpy(dest, src, n);
