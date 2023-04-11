d0c474371fbf17f4e671c9de5ca684547ddbe4cf
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 13:46:57 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+*_strcpy  - copy string.
+*@dest: destination.
+*@src: source.
+* Return: copied string.
+*/
 char *_strcpy(char *dest, char *src)
 {
-return strcpy(dest, src);
+return (strcpy(dest, src));
 }
