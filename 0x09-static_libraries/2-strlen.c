e900f5d0f4d15c6428dbabdba82299fdd7954879
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 14:03:39 +0300

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/** _strlen - returns length of a string
+/**
+*_strlen - returns length of a string
 *@str: String to be checked.
 *Return: Length of str.
 */
