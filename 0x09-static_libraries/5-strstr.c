98d1df23f4534dbd12df04d24c085523161dc420
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 13:36:30 +0300

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+**_strstr - Entry point.
+*@haystack: input.
+*@needle: input
+*Return: 0.
+*/
 char *_strstr(char *haystack, char *needle)
 {
 return strstr(haystack, needle);
