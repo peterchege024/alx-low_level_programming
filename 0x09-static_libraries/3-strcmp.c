98053bd1ad4874940d2d6b8989de0c994b773d20
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-11 13:20:30 +0300

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+**_strcmp - compares two strings.
+*@s1: first string.
+*@s2: second string.
+*Return: the difference.
+*/
 int _strcmp(char *s1, char *s2)
 {
-return strcmp(s1, s2);
+return (strcmp(s1, s2));
 }
