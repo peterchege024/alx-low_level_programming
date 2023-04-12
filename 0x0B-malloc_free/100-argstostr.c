68148a81ee60fceb4f4e09f1c6d3c1a59e994b3e
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-13 02:16:01 +0300

    advavcemalloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,80 +1,37 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
 /**
-		
- * argstostr - main entry
-		
- * @ac: int input
-		
- * @av: double pointer array
-		
- * Return: 0
-		
- */
-		
+* argstostr - main entry
+* @ac: input
+* @av: double
+* Return: 0
+*/		
 char *argstostr(int ac, char **av)
-		
 {
-		
-	int i, n, r = 0, l = 0;
-		
-	char *str;
-		
-
-		
-	if (ac == 0 || av == NULL)
-		
-		return (NULL);
-		
-
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-		for (n = 0; av[i][n]; n++)
-		
-			l++;
-		
-	}
-		
-	l += ac;
-		
-
-		
-	str = malloc(sizeof(char) * l + 1);
-		
-	if (str == NULL)
-		
-		return (NULL);
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-	for (n = 0; av[i][n]; n++)
-		
-	{
-		
-		str[r] = av[i][n];
-		
-		r++;
-		
-	}
-		
-	if (str[r] == '\0')
-		
-	{
-		
-		str[r++] = '\n';
-		
-	}
-		
-	}
-		
-	return (str);
-		
+int j, k, m, z = 0;
+char *str;
+if (ac == 0 || av == NULL)
+return (NULL);
+for (j = 0; j < ac; j++)
+{
+for (k = 0; av[j][k]; k++)
+z++;
 }
-
+z += ac;
+str = malloc(sizeof(char) * z + 1);
+if (str == NULL)
+return (NULL);
+for (j = 0; j < ac; j++)
+{
+for (k = 0; av[j][k]; k++)
+{
+str[m] = av[j][k];
+m++;
+}
+if (str[m] == '\0')
+{
+str[m++] = '\n';
+}
+}
+return (str);	
+}
