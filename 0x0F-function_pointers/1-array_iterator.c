12831eae7af1c9522714bcb0aab1da9b0deb5f37
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-21 14:53:58 +0300

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,19 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * array_iterator - prints each array elem on a newl
- * @array: array
- * @size: how many elem to print
- * @action: pointer to print in regular or hex
- * Return: void
- */
+* array_iterator - prints each array elem.
+* @array: array
+* @size: elemts to print
+* @action: print in regular or hex
+* Return: void
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
-	if (array == NULL || action == NULL)
-		return;
-
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
+unsigned int r;
+if (array == NULL || action == NULL)
+return;
+for (r = 0; r < size; r++)
+{
+action(array[r]);
 }
-
+}
