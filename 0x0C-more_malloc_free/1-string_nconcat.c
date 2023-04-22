ea42c42b99801c7c31d6865471845a92ec60d293
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-22 09:07:43 +0300

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,35 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
- * Return: pointer to the resulting string
- */
+* *string_nconcat - concatenates n bytes of a string to other string
+* @s1: first string
+* @s2: second string
+* @n: number of bytes from s2 to concatenate to s1
+* Return: pointer to the resulting string
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (!s)
-		return (NULL);
-
-	while (i < len1)
-	{
-		s[i] = s1[i];
-		i++;
-	}
-
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
-
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-
-	s[i] = '\0';
-
-	return (s);
+char *s;
+unsigned int y = 0, z = 0, ln1 = 0, ln2 = 0;
+while (s1 && s1[ln1])
+ln1++;
+while (s2 && s2[ln2])
+ln2++;
+if (n < ln2)
+s = malloc(sizeof(char) * (ln1 + n + 1));
+else
+s = malloc(sizeof(char) * (ln1 + ln2 + 1));
+if (!s)
+return (NULL);
+while (y < ln1)
+{
+s[y] = s1[y];
+y++;
 }
-
+while (n < ln2 && y < (ln1 + n))
+s[y++] = s2[z++];
+while (n >= ln2 && i < (ln1 + ln2))
+s[y++] = s2[z++];
+s[y] = '\0';
+return (s);
+}
