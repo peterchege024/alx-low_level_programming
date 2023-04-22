6d7414ae96edf09442f7bf8e433a65a29aa65cc2
peterchege024 <125446497+peterchege024@users.noreply.github.com>
2023-04-22 10:39:43 +0300

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,74 +1,55 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
+* _atoi - converts a string to integer
+* @s: string
+* Return: the int converted from the string
+*/
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+int j, k, l, ln, m, dgt;
+j = 0; k = 0; l = 0; ln = 0; m = 0; dgt = 0;
+while (s[ln] != '\0')
+ln++;
+while (j < ln && m == 0)
+{
+if (s[j] == '-')
+++k;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+if (s[j] >= '0' && s[j] <= '9')
+{
+dgt = s[j] - '0';
+if (k % 2)
+dgt = -dgt;
+l = l * 10 + dgt;
+m = 1;
+if (s[j + 1] < '0' || s[j + 1] > '9')
+break;
+m = 0;
 }
-
+j++;
+}
+if (m == 0)
+return (0);
+return (l);
+}
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 (Success), 1 (Error)
+*/
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
-
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
-	return (0);
+int rst, nm1, nm2;
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
 }
-
+nm1 = _atoi(argv[1]);
+nm2 = _atoi(argv[2]);
+rst = nm1 * nm2;
+printf("%d\n", rst);
+return (0);
+}
